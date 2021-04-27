3dfacae474db1f9c8e0002a12550925cb74d6ce1
Hel0909 <83198879+Hel0909@users.noreply.github.com>
2021-04-27 10:55:17 +0500

    12345

---
+++ astr/script.js
@@ -0,0 +1,5 @@
+var words = [`white`, `go`, `me`, `hello`, `family`]
+const show = (first, second) => {
+    return first[second]
+}
+console.log(show(words, 3))
---
+++ nw.html/script.js
@@ -0,0 +1,28 @@
+console.log("Bugun Imtihon")
+var a = "Hidoyathon"
+console.log(`Mening ismim ${a}`)
+
+
+
+var b = 100
+var d = 200
+console.log(`${b} ${d}`)
+
+
+
+var kil = 2000
+console.log(`${kil}` /1.6)
+
+
+
+var e = 10
+var f = 30
+if ( e > f ) {
+    console.log("E Fdan katta")
+}
+else if( e < f) {
+    console.log("E F dan kichkina")
+}
+else {
+    console.log("HATO")
+}
